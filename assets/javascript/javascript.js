19bc778420aa929e745e656f555c7e150a78359a
melissa rice <rice.jean29@gmail.com>
2018-02-17 16:13:03 -0700

    hangman_v.2

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,22 @@
+function myFunction(event) {
+    var x = event.key;
+
+    // If the pressed keyboard button is "a" or "A" (using caps lock or shift), alert some text.
+    
+    
+        console.log (x);
+    
+}
+
+
+//check if the letter the person typed is in the word 
+    //if it is
+        // play right chime **
+        // put it in the blank 
+        // reduce the number of letters left to guess 
+        // add +1 to letters guessed 
+    // if is not 
+        //play wrong chime **
+        //add +1 to letters guessed
+        //print the letter in the letters guessed box
+        
